233761048fdea27c314656f02b866405f720ffcb
Abhishek Thakur <abhishekthakurbir@gmail.com>
2019-10-16 00:57:50 +0530

    Added firebase configuration and added matches related components

--- src/Components/home/home.component.jsx
+++ src/Components/home/home.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Featured from "../featured/featured.component";
+import MatchesHome from "../matches/matches-home.component";
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
+      <MatchesHome />
     </div>
   );
 };
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2aP7xDzCI4OFABqlWH3d9GqSfH5BhZME",
+  authDomain: "react-fcb.firebaseapp.com",
+  databaseURL: "https://react-fcb.firebaseio.com",
+  projectId: "react-fcb",
+  storageBucket: "react-fcb.appspot.com",
+  messagingSenderId: "365141335032",
+  appId: "1:365141335032:web:4b640713eaf9a3b4bc940d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firebaseDB = firebase.database();
+const firebaseMatches = firebaseDB.ref("matches");
+
+export { firebase, firebaseMatches };
