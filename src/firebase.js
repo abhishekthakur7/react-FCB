41a9c7d2de841b083a7b4d9350d9a0c3b15a2a63
Abhishek Thakur <abhishekthakurbir@gmail.com>
2019-10-20 18:59:28 +0530

    Added admin players & matches logic

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/app";
 import "firebase/database";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD2aP7xDzCI4OFABqlWH3d9GqSfH5BhZME",
@@ -17,6 +18,15 @@ firebase.initializeApp(firebaseConfig);
 
 const firebaseDB = firebase.database();
 const firebaseMatches = firebaseDB.ref("matches");
-const firebasePromotions = firebaseDB.ref('promotions');
+const firebasePromotions = firebaseDB.ref("promotions");
+const firebaseTeams = firebaseDB.ref("teams");
+const firebasePlayers = firebaseDB.ref("players");
 
-export { firebase, firebaseMatches, firebasePromotions };
+export {
+  firebase,
+  firebaseDB,
+  firebaseMatches,
+  firebasePromotions,
+  firebaseTeams,
+  firebasePlayers
+};
--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,55 @@ import PublicRoute from "./Components/auth-routes/public-routes.component";
 
 import Home from "./Components/home/home.component";
 import SignIn from "./Components/sign-in/sign-in.component";
+import TheTeam from "./Components/the-team/the-team.component";
+
 import Dashboard from "./Components/admin/dashboard.component";
+import AdminMatches from "./Components/admin/matches/admin-matches.component";
+import AddEditMatch from "./Components/admin/matches/add-edit-match.component";
+import AdminPlayers from "./Components/admin/players/admin-players.component";
+import AddEditPlayers from "./Components/admin/players/add-edit-players.component";
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute
+          {...props}
+          path="/admin_players/add_players"
+          exact
+          component={AddEditPlayers}
+        />
+        <PrivateRoute
+          {...props}
+          path="/admin_players/add_players/:id"
+          exact
+          component={AddEditPlayers}
+        />
+        <PrivateRoute
+          {...props}
+          path="/admin_players"
+          exact
+          component={AdminPlayers}
+        />
+        <PrivateRoute
+          {...props}
+          path="/admin_matches/edit_match"
+          exact
+          component={AddEditMatch}
+        />
+        <PrivateRoute
+          {...props}
+          path="/admin_matches/edit_match/:id"
+          exact
+          component={AddEditMatch}
+        />
+        <PrivateRoute
+          {...props}
+          path="/admin_matches"
+          exact
+          component={AdminMatches}
+        />
+
         <PrivateRoute
           {...props}
           path="/dashboard"
@@ -24,6 +67,13 @@ const Routes = props => {
           path="/sign_in"
           exact
           component={SignIn}
+        />
+        <PublicRoute
+          {...props}
+          restricted={false}
+          path="/the_team"
+          exact
+          component={TheTeam}
         />
         <PublicRoute
           {...props}
